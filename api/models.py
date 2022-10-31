0e61d3e53400f075f2a841d06dce8f980ff1ee9f
Marrowsed <marcus.ah@gmail.com>
2022-10-31 12:46:53 -0300

    category model

--- api/models.py
+++ api/models.py
@@ -3,9 +3,22 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False)
+    color = models.CharField(max_length=10, null=False, blank=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Categorie"
+
+
 class Video(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
     url = models.URLField(max_length=200, null=False, blank=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=1, null=False, blank=False)
 
-
+    def __str__(self):
+        return self.title

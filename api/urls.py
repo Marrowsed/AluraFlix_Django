a03bb6e22056c70aa3fa8e21058e55de4f82b9d2
Marrowsed <marcus.ah@gmail.com>
2022-10-31 12:47:58 -0300

    category urls

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('videos', VideoView.as_view(), name="video-view"),
+    path('videos/', VideoView.as_view(), name="video-view"),
     path('videos/<str:pk>/', VideoDetail.as_view(), name="video-detail"),
+    path('categories/', CategoryView.as_view(), name='category-view'),
+    path('categories/<str:pk>/', CategoryDetail.as_view(), name='category-detail'),
+    path('categories/<str:pk>/videos/', video_category_detail, name='video-category-detail')
 
 
 ]

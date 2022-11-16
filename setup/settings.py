e00378c7713fe42c7fbdeb55e98650daaef27884
Marrowsed <marcus.ah@gmail.com>
2022-11-16 07:43:43 -0300

    Feat: Increasing JWT expire minutes

--- setup/settings.py
+++ setup/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
